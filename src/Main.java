06d547937b57f96e1e40080cf2f0e3ee97c2ee00
Laptev <sl260401@gmail.com>
2021-10-04 22:45:04 +0300

    V1

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("lab 1.fxml"));
+            Parent root = loader.load();
+
+            primaryStage.getIcons().add(new Image("icon.png"));
+            primaryStage.setTitle("Работа с изображениями");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println("Ошибка открытия главного окна");
+        }
+    }
+}
